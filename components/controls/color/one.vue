add14857f47df984521f52160da54eae5f2d79bd
Mik <info@slnt-opp.xyz>
2020-12-22 12:27:01 +0200

    color picker components

---
+++ components/controls/color/one.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    :style="{
+      borderRadius: '50%',
+      width: '3rem',
+      height: '3rem',
+      borderWidth: '5px',
+      borderColor: 'black',
+      borderStyle: 'solid',
+      margin: '3px',
+      backgroundColor: color,
+    }"
+    v-on:click="$emit('click')"
+  ></div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      required: true,
+      type: String,
+    },
+  },
+}
+</script>
---
+++ components/controls/color/palette.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-row style="background-color: white; border-radius: 10px">
+    <template v-if="horizontal">
+      <a-col v-for="color in colors" :key="color" :span="4">
+        <one :color="color" @click="$emit('click', color)" />
+      </a-col>
+    </template>
+    <a-col v-else :span="24">
+      <one
+        v-for="color in colors"
+        :key="color"
+        :color="color"
+        @click="$emit('click', color)"
+      />
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+import one from './one.vue'
+export default {
+  components: { one },
+  props: {
+    horizontal: {
+      default: false,
+      type: Boolean,
+    },
+    colors: {
+      required: true,
+      type: Array,
+    },
+  },
+}
+</script>
