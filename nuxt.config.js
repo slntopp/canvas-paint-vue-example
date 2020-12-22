eda673ada8eeef9db844d31cfec69b9efc2e7d30
Mik <info@slnt-opp.xyz>
2020-12-22 11:23:38 +0200

    antd

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: ["@/plugins/antd-ui"],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -32,5 +32,13 @@ export default {
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    loaders: {
+     less: {
+       lessOptions: {
+         javascriptEnabled: true,
+       },
+      },
+    },
+  },
 }
